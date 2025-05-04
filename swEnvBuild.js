790fb14292226687f4332d3579d85c31d7522078
M-Ali-Haider <mah01134212100@gmail.com>
2025-05-04 16:22:54 +0500

    bugfix: fixing swEnv build.js

--- swEnvBuild.js
+++ swEnvBuild.js
@@ -14,7 +14,7 @@ const {
 } = process.env;
 
 // Create the content for the swEnv.js file, injecting environment variables
-const content = `const swEnv = {
+const content = `var swEnv = {
   NEXT_PUBLIC_FIREBASE_API_KEY: '${NEXT_PUBLIC_FIREBASE_API_KEY}', 
   NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: '${NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}',
   NEXT_PUBLIC_FIREBASE_PROJECT_ID: '${NEXT_PUBLIC_FIREBASE_PROJECT_ID}',
@@ -22,8 +22,6 @@ const content = `const swEnv = {
   NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: '${NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}',
   NEXT_PUBLIC_FIREBASE_APP_ID: '${NEXT_PUBLIC_FIREBASE_APP_ID}',
 };
-
-export default swEnv;
 `;
 
 // Write the swEnv.js file to the public directory
